333c98863af2ab85ed511fc38b2116f7f22fdbd6
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-01-24 13:19:59 -0300

    Add swagger to api

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-require('dotenv').config()
-const express = require("express");
+require("dotenv").config();
 const routes = require("./src/routes/index.routes");
+const swaggerUI = require("swagger-ui-express");
 const db = require("./src/config/db.js");
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,17 +11,19 @@ const PORT = process.env.PORT || 8877;
 
 app.use(cors());
 routes(app);
-if(process.env.NODE_ENV !==  'test'){
-    app.listen(PORT, ()=>{
-        console.log(`Servidor rodando na porta ${PORT}`);
-    });
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
 }
+app.use("/", swaggerUI.serve, swaggerUI.setup(require("./swagger.json")));
+db.on(
+  "error",
+  console.error.bind("Houver um erro ao conectar o banco de dados")
+);
 
-db.on("error",console.error.bind("Houver um erro ao conectar o banco de dados"));
-
-
-db.once("open", ()=>{
-    console.log("Conexão com banco de dados feita com sucesso");
-})
+db.once("open", () => {
+  console.log("Conexão com banco de dados feita com sucesso");
+});
 
 module.exports = app;
